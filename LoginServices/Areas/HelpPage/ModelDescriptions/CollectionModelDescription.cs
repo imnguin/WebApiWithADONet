96e1e350d777466b18f433505fdc4bc980f89dc5
Lâm Xuân Nguyên <lamnguyencoder@gmail.com>
2022-05-16 11:37:45 +0700

    demo web api with ado net

---
+++ LoginServices/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LoginServices.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ LoginServices/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using LoginDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoginServices.Controllers
+{
+    public class LoginController : ApiController
+    {
+        LoginDetailsEntities entities = new LoginDetailsEntities();
+
+        [HttpGet]
+        public HttpResponseMessage GetAllUser()
+        {
+            var list = entities.User.ToList();
+            if (list != null)
+                return Request.CreateResponse(HttpStatusCode.OK, list);
+            else return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+        [HttpGet]
+        public HttpResponseMessage GetUserById(int id)
+        {
+            var user = entities.User.FirstOrDefault(e => e.Id == id);
+            if (user != null)
+                return Request.CreateResponse(HttpStatusCode.OK, user);
+            else return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+    }
+}
